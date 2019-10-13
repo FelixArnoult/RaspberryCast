9ff0967ebbb8e563f4c604d49e6f96bf1d9a1a46
Félix Arnoult <fel.arnoult@gmail.com>
2019-10-13 20:10:31 +0200

    aze

--- playerManager.py
+++ playerManager.py
@@ -12,31 +12,31 @@ class Singleton(type):
         return cls._instances[cls]
 
 class PlayerManager(OMXPlayer, metaclass=Singleton):
-    self.SHORT_SEEK = 10
-    self.LONG_SEEK = 60
-    self.VOLUME_STEP = 0.5
+    SHORT_SEEK = 10
+    LONG_SEEK = 60
+    VOLUME_STEP = 0.5
     def __init__(self):
         STREAM_URI = './video.mp4'
         OMXPlayer.__init__(self, STREAM_URI)
 
     def decrease_volume(self):
-        self.set_volume(self.volume() - self.VOLUME_STEP)
+        self.set_volume(self.volume() - VOLUME_STEP)
 
     def increase_volume(self):
-        self.set_volume(self.volume() + self.VOLUME_STEP)
+        self.set_volume(self.volume() + VOLUME_STEP)
 
-    def short_forward(self): #TODO check when remain duration < SHORT_SEEK
+    def short_forward(self): #TODO check when remain duratiSHORT_SEEK
         if(self.can_seek()):
-            self.seek(self.position() + self.SHORT_SEEK)
+            self.seek(self.position() + SHORT_SEEK)
 
     def short_backward(self):
         if(self.can_seek()):
-            self.seek(self.position() - self.SHORT_SEEK)
+            self.seek(self.position() - SHORT_SEEK)
 
-    def long_forward(self): #TODO check when remain duration < self.SHORT_SEEK
+    def long_forward(self): #TODO check when remain duration < SHORT_SEEK
         if(self.can_seek()):
-            self.seek(self.position() + self.SHORT_SEEK)
+            self.seek(self.position() + LONG_SEEK)
 
     def long_backward(self):
         if(self.can_seek()):
-            self.seek(self.position() - self.SHORT_SEEK)
+            self.seek(self.position() - LONG_SEEK)
